5ce30dd1c8ea8495bd2cd194b3a1257335cfb45f
Alexander Lyon <arlyon@me.com>
2024-11-13 14:57:00 +0000

    check edge

--- my-app/src/app/edge/route.ts
+++ my-app/src/app/edge/route.ts
@@ -1,14 +1,14 @@
-import { geolocation } from "@vercel/functions";
+// import { geolocation } from "@vercel/functions";
 import { type NextRequest, NextResponse } from "next/server";
 
-const { Buffer } = require("buffer");
+// const { Buffer } = require("buffer");
 
 export function GET(request: NextRequest): NextResponse {
-  const geoCountry = geolocation(request).country;
+  // const geoCountry = geolocation(request).country;
   return NextResponse.json({
-    geoCountry,
+    // geoCountry,
     acceptLanguage: request.headers.get("accept-language"),
-    buffer: Buffer.from([1, 2]).toString("hex"),
+    // buffer: Buffer.from([1, 2]).toString("hex"),
   });
 }
 
